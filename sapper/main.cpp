cc064a0ac63299a1d486c601ab491b7401a8df86
Babuleh78 <150275581+Babuleh78@users.noreply.github.com>
2024-06-03 15:52:28 +0300

    Last change
    Примитивное меню. Для выхода в меню нажимать esc

--- sapper/main.cpp
+++ sapper/main.cpp
@@ -101,7 +101,6 @@ int min_count(cell(*field)[30], int x, int y, int width, int height) {
 	else {
 		return field[x - 1][y].get_condition() + field[x - 1][y - 1].get_condition() + field[x - 1][y + 1].get_condition() + field[x][y - 1].get_condition() + field[x][y + 1].get_condition() + field[x + 1][y - 1].get_condition() + field[x + 1][y + 1].get_condition() + field[x + 1][y].get_condition();
 	}
-	cout << "Обосрался";
 	return 5678;
 }
 void show_mas(cell(*field)[30], int width, int height) {
@@ -220,11 +219,12 @@ int main() {
 	SetTraceLogLevel(LOG_NONE);
 	bool first_klick = true, alive = true;
 	int start_x, start_y;
+	int hard_level = 6;
 	setlocale(LC_ALL, "Russian");
 	const int screenWidth = 800;
 	const int screenHeight = 450;
-	int width = 30;
-	int height = 30;
+	int width = 15;
+	int height = 15;
 	int numMines = 0;
 	int a = 0;
 	InitWindow(screenWidth, screenHeight, "Sapper");;
@@ -240,7 +240,7 @@ int main() {
 	Image image6 = LoadImage("Image/6.png");ImageResize(&image6, 50, 50);
 	Image image7 = LoadImage("Image/7.png");ImageResize(&image7, 50, 50);
 	cell field[30][30];
-
+	int menuOption = 0;
 	Rectangle rec{ 300, 200, 200, 50 };
 	
 	Camera2D camera = { 0 };
@@ -255,227 +255,352 @@ int main() {
 	bool start = true;
 	while (!WindowShouldClose())        
 	{
-		if (first_klick) {
-			alive = true;
-		}
-		Vector2 Cursor = GetMousePosition();
-		if (!alive) {
-			if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-				
-				if ((Cursor.x >= screenWidth / 2 - 55) && (Cursor.x <= screenWidth / 2 +155) && (Cursor.y >=   screenHeight / 2) && (Cursor.y <=  screenHeight / 2 + 50)) {
-					
-					numMines = 0;
-					for (int i = 0; i < height; i++) {
-						for (int j = 0; j < width; j++) {
-							field[i][j].set_open(false);
-						}
-					}
-					
-					first_klick = true;
-				}
-			}
-			
-		}
-		if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		DrawText("MENU", screenWidth/2 - 100, 80, 40, BLACK);
+
+		// Отображение пунктов меню
+		DrawText(menuOption == 0 ? "> Play" : "Play", screenWidth/2 -100 , 150, 40, BLACK);
+		DrawText(menuOption == 1 ? "> Choose difficulty" : "Choose difficulty", screenWidth/2 -100, 190, 40, BLACK);
+		DrawText(menuOption == 2 ? "> Choose a size" : "Choose a size", screenWidth/2 -100, 230, 40, BLACK);
+		DrawText(menuOption == 3 ? "> Exit" : "Exit", screenWidth/2 -100, 270, 40, BLACK);
+
+		// Обработка навигации по меню
+		if (IsKeyPressed(KEY_UP)) menuOption--;
+		if (IsKeyPressed(KEY_DOWN)) menuOption++;
+		if (menuOption < 0) menuOption = 3;
+		if (menuOption > 3) menuOption = 0;
+
+		// Обработка выбора пункта меню
+		if (IsKeyPressed(KEY_ENTER))
 		{
-			Vector2 delta = GetMouseDelta();
-			delta = Vector2Scale(delta, -1.0f / camera.zoom);
-			
-			camera.target = Vector2Add(camera.target, delta);
-			
-			pointX += delta.x; pointY += delta.y;
-		}
-		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && alive) {
-			if (!first_klick) {
-				
-				x_cell = (Cursor.x + pointX); x_cell /= 50;
-				y_cell = (Cursor.y + pointY); y_cell /= 50;
-
-				x_int_cell = (int)x_cell; y_int_cell = (int)y_cell;
-				if (x_cell >= 0 && y_cell >= 0) {
-					int j = x_int_cell, i = y_int_cell;
-					if (field[j][i].get_condition() != -1) {
-						if (field[j][i].get_condition() == 1) {
-							field[j][i].set_texture(mine);
-							field[j][i].set_open(true);
-							alive = false;
-						}
-						else {
-							a = field[j][i].get_around();
-							if (a != 0)field[j][i].set_open(true);
-							switch (a) {
-							case 0:
-								field[j][i].set_texture(open_image);
-								openEmptyCells(field, j, i, width, height);
-								break;
-							case 1:
-								field[j][i].set_texture(image1);  break;
-							case 2:
-								field[j][i].set_texture(image2);break;
-							case 3:
-								field[j][i].set_texture(image3);break;
-							case 4:
-								field[j][i].set_texture(image4);break;
-							case 5:
-								field[j][i].set_texture(image5);break;
-							case 6:
-								field[j][i].set_texture(image6);break;
-							case 7:
-								field[j][i].set_texture(image7);break;
-
-							}
-							if (a != 0) {
-								field[j][i].set_open(true);
-							}
-						}
+			if (menuOption == 0)
+			{
+				while (!WindowShouldClose()) {
+					if (first_klick) {
+						alive = true;
 					}
+					Vector2 Cursor = GetMousePosition();
+					if (!alive) {
+						if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
 
-				}
-			}
-			else {
-				x_cell = (Cursor.x + pointX); x_cell /= 50;
-				y_cell = (Cursor.y + pointY); y_cell /= 50;
-				if (y_cell >= 0 && x_cell >= 0) {
-					start_x = (int)x_cell; start_y = (int)y_cell;
-					for (int i = 0; i < height; i++) {
-						for (int j = 0; j < width; j++) {
-							if (start_x - 1 <= j && j <= start_x + 1 && start_y - 1 <= i && i <= start_y + 1) {
-								field[j][i].set_condition(0);
+							if ((Cursor.x >= screenWidth / 2 - 55) && (Cursor.x <= screenWidth / 2 + 155) && (Cursor.y >= screenHeight / 2) && (Cursor.y <= screenHeight / 2 + 50)) {
+
+								numMines = 0;
+								for (int i = 0; i < height; i++) {
+									for (int j = 0; j < width; j++) {
+										field[i][j].set_open(false);
+									}
+								}
+
+								first_klick = true;
 							}
-							else {
-								a = rand() % 6; if (a != 1) { a = 0; }
-								field[j][i].set_condition(a);
-								if (a == 1) {
-									numMines += 1;
+						}
+
+					}
+					if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+					{
+						Vector2 delta = GetMouseDelta();
+						delta = Vector2Scale(delta, -1.0f / camera.zoom);
+
+						camera.target = Vector2Add(camera.target, delta);
+
+						pointX += delta.x; pointY += delta.y;
+					}
+					if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && alive) {
+						if (!first_klick) {
+
+							x_cell = (Cursor.x + pointX); x_cell /= 50;
+							y_cell = (Cursor.y + pointY); y_cell /= 50;
+
+							x_int_cell = (int)x_cell; y_int_cell = (int)y_cell;
+							if (x_cell >= 0 && y_cell >= 0) {
+								int j = x_int_cell, i = y_int_cell;
+								if (field[j][i].get_condition() != -1) {
+									if (field[j][i].get_condition() == 1) {
+										field[j][i].set_texture(mine);
+										field[j][i].set_open(true);
+										alive = false;
+									}
+									else {
+										a = field[j][i].get_around();
+										if (a != 0)field[j][i].set_open(true);
+										switch (a) {
+										case 0:
+											field[j][i].set_texture(open_image);
+											openEmptyCells(field, j, i, width, height);
+											break;
+										case 1:
+											field[j][i].set_texture(image1);  break;
+										case 2:
+											field[j][i].set_texture(image2);break;
+										case 3:
+											field[j][i].set_texture(image3);break;
+										case 4:
+											field[j][i].set_texture(image4);break;
+										case 5:
+											field[j][i].set_texture(image5);break;
+										case 6:
+											field[j][i].set_texture(image6);break;
+										case 7:
+											field[j][i].set_texture(image7);break;
+
+										}
+										if (a != 0) {
+											field[j][i].set_open(true);
+										}
+									}
 								}
 
 							}
 						}
+						else {
+							x_cell = (Cursor.x + pointX); x_cell /= 50;
+							y_cell = (Cursor.y + pointY); y_cell /= 50;
+							if (y_cell >= 0 && x_cell >= 0) {
+								start_x = (int)x_cell; start_y = (int)y_cell;
+								for (int i = 0; i < height; i++) {
+									for (int j = 0; j < width; j++) {
+										if (start_x - 1 <= j && j <= start_x + 1 && start_y - 1 <= i && i <= start_y + 1) {
+											field[j][i].set_condition(0);
+										}
+										else {
+											a = rand() % hard_level; if (a != 1) { a = 0; }
+											field[j][i].set_condition(a);
+											if (a == 1) {
+												numMines += 1;
+											}
+
+										}
+									}
+								}
+
+
+								for (int i = 0; i < height; i++) {
+									for (int j = 0; j < width; j++) {
+										a = min_count(field, j, i, width, height);
+										field[j][i].set_around(a);
+										switch (a) {
+										case 0:
+											if (field[j][i].get_condition() != 1) {
+												field[j][i].set_image(open_image);
+											}
+											else {
+												field[j][i].set_image(mine);
+
+											}
+											break;
+
+										case 1:
+											field[j][i].set_image(image1); break;
+										case 2:
+											field[j][i].set_image(image2); break;
+										case 3:
+											field[j][i].set_image(image3); break;
+										case 4:
+											field[j][i].set_image(image4); break;
+										case 5:
+											field[j][i].set_image(image5); break;
+										case 6:
+											field[j][i].set_image(image6); break;
+										case 7:
+											field[j][i].set_image(image7); break;
+
+										}
+
+									}
+								}
+								field[start_x][start_y].set_around(0); field[start_x][start_y].set_image(open_image);
+								openEmptyCells(field, start_x, start_y, width, height);
+								first_klick = false;
+							}
+						}
+
 					}
+					if (IsMouseButtonPressed(MOUSE_MIDDLE_BUTTON) && alive && !first_klick) {
+						x_cell = (Cursor.x + pointX); x_cell /= 50;
+						y_cell = (Cursor.y + pointY); y_cell /= 50;
+
+						x_int_cell = (int)x_cell; y_int_cell = (int)y_cell;
+						if (x_int_cell >= 0 && y_int_cell >= 0) {
+							int j = x_int_cell, i = y_int_cell;
+							if (field[j][i].get_condition() != -1 && !field[j][i].is_open()) { // обезвреживание
+								field[j][i].set_texture(flag);
+								field[j][i].set_condition(-1);
+								field[j][i].set_open(true);
+								numMines -= 1;
+
+							}
+							else if (field[j][i].get_condition() == -1) { //отмена обезвреда
 
 
-					for (int i = 0; i < height; i++) {
-						for (int j = 0; j < width; j++) {
-							a = min_count(field, j, i, width, height);
-							field[j][i].set_around(a);
-							switch (a) {
-							case 0:
-								if (field[j][i].get_condition() != 1) {
-									field[j][i].set_image(open_image);
+								if (field[j][i].get_around() != 0) {
+									field[j][i].set_condition(0);
+									field[j][i].set_texture(image_number_set(field[j][i].get_around()));
 								}
 								else {
-									field[j][i].set_image(mine);
-
+									field[j][i].set_condition(1);
+									field[j][i].set_texture(mine);
 								}
-								break;
+								field[j][i].set_open(false);
+								numMines += 1;
 
-							case 1:
-								field[j][i].set_image(image1); break;
-							case 2:
-								field[j][i].set_image(image2); break;
-							case 3:
-								field[j][i].set_image(image3); break;
-							case 4:
-								field[j][i].set_image(image4); break;
-							case 5:
-								field[j][i].set_image(image5); break;
-							case 6:
-								field[j][i].set_image(image6); break;
-							case 7:
-								field[j][i].set_image(image7); break;
+							}
+							else if (field[j][i].get_condition() == 0) {
+								a = field[j][i].get_around();
+								int count = 0; around_check(field, j, i, width, height, count, true);
+								if (count == a) {
+									open_around(field, j, i, width, height, true, alive);
+								}
 
 							}
 
 						}
+
 					}
-					field[start_x][start_y].set_around(0); field[start_x][start_y].set_image(open_image);
-					openEmptyCells(field, start_x, start_y, width, height);
-					first_klick = false;
+
+
+
+					BeginDrawing();
+					ClearBackground(RAYWHITE);
+					BeginMode2D(camera);
+
+					if (numMines >= 100) {
+						string s = to_string(numMines);
+
+						bombs[11] = s[0]; bombs[12] = s[1]; bombs[13] = s[2]; bombs[14] = '\0';
+					}
+					else if (numMines >= 10) {
+						string s = to_string(numMines);
+
+						bombs[11] = s[0]; bombs[12] = s[1]; bombs[13] = '\0';
+					}
+					else {
+						bombs[11] = numMines + '0'; bombs[12] = '\0';
+					}
+					for (int i = 0;i < height; i++) {
+						for (int j = 0; j < width; j++) {
+							DrawTexture(field[j][i].get_texture(), j * 50, i * 50, WHITE);
+
+						}
+					}
+
+					if (!alive) {
+						DrawText(bombs, pointX, pointY, 50, RED);
+						rec.x = pointX + screenWidth / 2 - 55; rec.y = pointY + screenHeight / 2;
+						DrawRectangleRec(rec, GREEN);
+						DrawText("AGAIN?", pointX + screenWidth / 2 - 50, pointY + screenHeight / 2, 50, BLACK);
+					}
+					else {
+						DrawText(bombs, pointX, pointY, 50, RED);
+					}
+					EndDrawing();
+
+
 				}
 			}
-			
-		}
-		if (IsMouseButtonPressed(MOUSE_MIDDLE_BUTTON) && alive && !first_klick) {
-			x_cell = (Cursor.x + pointX); x_cell /= 50;
-			y_cell = (Cursor.y + pointY); y_cell /= 50;
-			
-			x_int_cell = (int)x_cell; y_int_cell = (int)y_cell;
-			if (x_int_cell >= 0 && y_int_cell >= 0) {
-				int j = x_int_cell, i = y_int_cell;
-					if (field[j][i].get_condition() != -1 && !field[j][i].is_open()) { // обезвреживание
-						field[j][i].set_texture(flag);
-						field[j][i].set_condition(-1);
-						field[j][i].set_open(true);
-						numMines -= 1;
-						
-					} 
-					else if (field[j][i].get_condition() == -1){ //отмена обезвреда
-						
-						
-						if (field[j][i].get_around() != 0) {
-							field[j][i].set_condition(0);
-							field[j][i].set_texture(image_number_set(field[j][i].get_around()));
-						}
-						else {
-							field[j][i].set_condition(1);
-							field[j][i].set_texture(mine);
-						}
-						field[j][i].set_open(false);
-						numMines += 1;
-						
+			else if (menuOption == 1)
+			{
+				while (!WindowShouldClose()) {
+					Vector2 Cursor = GetMousePosition();
+					DrawText("Difficulty:", 150, 50, 70, BLACK);
+					if (hard_level == 6) {
+						DrawText("Easy", 500, 50, 70, BLACK);
 					}
-					else if (field[j][i].get_condition() == 0) {
-						a = field[j][i].get_around();
-						int count = 0; around_check(field, j, i, width, height, count, true);
-						if (count == a) {
-							open_around(field, j, i, width, height, true, alive);
-						}
-						
+					else if (hard_level == 5) {
+						DrawText("Medium", 500, 50, 70, BLACK);
 					}
-				
-			}
-
-		}
-		
-		
-			
-			BeginDrawing();
-			ClearBackground(RAYWHITE);
-			BeginMode2D(camera);
-			
-			if (numMines >= 100) {
-				string s = to_string(numMines);
-				
-				bombs[11] = s[0]; bombs[12] = s[1]; bombs[13] = s[2]; bombs[14] = '\0';
-			}
-			else if (numMines >= 10) {
-				string s = to_string(numMines);
-				
-				bombs[11] = s[0]; bombs[12] = s[1]; bombs[13] = '\0';
-			}
-			else {
-				bombs[11] = numMines + '0'; bombs[12] = '\0';
-			}
-			for (int i = 0;i < 30; i++) {
-				for (int j = 0; j < 30; j++) {
-					DrawTexture(field[j][i].get_texture(), j * 50, i * 50, WHITE);
+					else {
+						DrawText("Hard", 500, 50, 70, BLACK);
+					}
 					
-				} 
+					BeginDrawing();
+					ClearBackground(WHITE);
+
+					rec.x = 10; rec.y = 175;
+					rec.width = 150;
+					DrawRectangleRec(rec, GREEN);
+					rec.x = 300; rec.y = 175;
+					rec.width = 210;
+					DrawRectangleRec(rec, YELLOW);
+					rec.x = 600; rec.y = 175;
+					rec.width = 150;
+					DrawRectangleRec(rec, RED);
+					DrawText("MEDIUM", 300, 175, 50, BLACK);
+					DrawText("EASY", 10, 175, 50, BLACK);
+					DrawText("HARD", 610, 175, 50, BLACK);
+					EndDrawing();
+					if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+						
+						if ((Cursor.x >= 10) && (Cursor.x <= 180) && (Cursor.y >= 175) && (Cursor.y <= 225)) {
+							hard_level = 6;
+						}
+						else if ((Cursor.x >= 300) && (Cursor.x <= 525) && (Cursor.y >= 175) && (Cursor.y <= 225)) {
+							hard_level = 5;
+						}
+						else if ((Cursor.x >= 610) && (Cursor.x <= 780) && (Cursor.y >= 175) && (Cursor.y <= 225)) {
+							hard_level = 4;
+						}
+					}
+				
+				}
 			}
-			
-			if (!alive) {
-				DrawText(bombs, pointX, pointY, 50, RED);
-				rec.x = pointX + screenWidth / 2 - 55; rec.y = pointY + screenHeight / 2;
-				DrawRectangleRec(rec, GREEN);
-				DrawText("AGAIN?", pointX+ screenWidth / 2 - 50, pointY + screenHeight / 2,  50, BLACK);
+			else if (menuOption == 2)
+			{
+				while (!WindowShouldClose()) {
+					Vector2 Cursor = GetMousePosition();
+					DrawText("Size:", 250, 50, 70, BLACK);
+					if (width == 30) {
+						DrawText("Big", 420, 50, 70, BLACK);
+					}
+					else if (width == 20) {
+						DrawText("Medium", 420, 50, 70, BLACK);
+					}
+					else {
+						DrawText("Small", 420, 50, 70, BLACK);
+					}
+
+					BeginDrawing();
+					ClearBackground(WHITE);
+
+					rec.x = 10; rec.y = 175;
+					rec.width = 180;
+					DrawRectangleRec(rec, GREEN);
+					rec.x = 300; rec.y = 175;
+					rec.width = 210;
+					DrawRectangleRec(rec, YELLOW);
+					rec.x = 600; rec.y = 175;
+					rec.width = 120;
+					DrawRectangleRec(rec, RED);
+					DrawText("MEDIUM", 300, 175, 50, BLACK);
+					DrawText("SMALL", 10, 175, 50, BLACK);
+					DrawText("BIG", 610, 175, 50, BLACK);
+					EndDrawing();
+					if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+
+						if ((Cursor.x >= 10) && (Cursor.x <= 180) && (Cursor.y >= 175) && (Cursor.y <= 225)) {
+							width = 15; height = 15;
+						}
+						else if ((Cursor.x >= 300) && (Cursor.x <= 525) && (Cursor.y >= 175) && (Cursor.y <= 225)) {
+							width = 20; height = 20;
+						}
+						else if ((Cursor.x >= 610) && (Cursor.x <= 780) && (Cursor.y >= 175) && (Cursor.y <= 225)) {
+							width = 30; height = 30;
+						}
+					}
+
+				}
 			}
-			else {
-				DrawText(bombs, pointX, pointY, 50, RED);
+			else if (menuOption == 3)
+			{
+				break; // Выход из цикла
 			}
-			EndDrawing();
+		}
+
+		EndDrawing();
 		
-		
-		//----------------------------------------------------------------------------------
 	}
 
 
